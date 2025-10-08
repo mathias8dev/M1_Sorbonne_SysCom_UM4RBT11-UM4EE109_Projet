8692dc32e1d2c35b262610d2e4262a38231293da
mathias8dev <kalipemathias.pro@gmail.com>
2025-10-08 17:08:52 +0200

    update: Hello pygame

--- app/src/main.py
+++ app/src/main.py
@@ -1,4 +1,16 @@
-
+def run():
+    import pygame
+    background_colour = (255,255,255)
+    (width, height) = (1024, 720)
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption('Projet Python')
+    screen.fill(background_colour)
+    pygame.display.flip()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
 if __name__ == "__main__":
-    print("Hello, World!")
+    run()
