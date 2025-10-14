e2a40898474030a3198e94bce2cb2370f1307e5d
mathias8dev <kalipemathias.pro@gmail.com>
2025-10-14 23:17:47 +0200

    update: Try to display images using 9 x 5 grid

--- app/src/main.py
+++ app/src/main.py
@@ -1,17 +1,34 @@
 def run():
     import pygame
+    import os
+    room_size = 90
     background_colour = (255,255,255)
-    (width, height) = (1800, 820)
+    (width, height) = (1800, room_size * 9 + 5)
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('Blue Prince by KALIPE | DIALLO | MOUSTADRANE')
     screen.fill(background_colour)
     left_side_color = (0, 0, 0) # La couleur gauche (noir)
     right_side_color = (255, 255, 255) # La couleur droite (blanc)
     
-    room_size = 140
     
-    pygame.draw.rect(screen, left_side_color, pygame.Rect(0, 0, room_size * 5, height))
-    pygame.draw.rect(screen, right_side_color, pygame.Rect(room_size * 5, 0, width - room_size * 5, height))
+    pygame.draw.rect(screen, left_side_color, pygame.Rect(0, 0, room_size * 5 + 5, height))
+    pygame.draw.rect(screen, right_side_color, pygame.Rect(room_size * 5 + 5, 0, width - (room_size * 5 + 5), height))
+    
+    app_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    assets_dir = os.path.join(app_dir, 'assets')
+    print(dir)
+    examples = [os.path.join(assets_dir, "rooms/001-012/Attic_Icon.webp") for i in range (9)]
+    for i in range(5):
+        loaded_image = pygame.transform.scale(pygame.image.load(examples[i]).convert(), (room_size, room_size))
+        
+        screen.blit(loaded_image, (i * room_size + 2, height - room_size - 2))
+        
+    for i in range(9):
+        loaded_image = pygame.transform.scale(pygame.image.load(examples[i]).convert(), (room_size, room_size))
+        
+        screen.blit(loaded_image, (0, room_size * i +2,))
+    
+    
     pygame.display.flip()
     running = True
     while running:
