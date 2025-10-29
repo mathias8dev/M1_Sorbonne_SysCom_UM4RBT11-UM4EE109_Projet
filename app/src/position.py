2c7c1c41ba46a7695d429a888b51c47138006c5c
moustadraneismael98 <Ismael.Moustadrane@etu.sorbonne-universite.fr>
2025-10-29 20:48:32 +0100

    Creation des classes

---
+++ app/src/position.py
@@ -0,0 +1,5 @@
+## Cette classe va gerer les delacement du joueur: Donc doit permettre de le positionner dans le manoir
+#  Elle peut aussi être utilisée pour positionner les rooms. 
+
+class position:
+    pass 
