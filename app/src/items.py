077e386fbdd2432d5cc317fae7a0667776fb020f
djouldediallodalein <etudeelectroniquesorbonne@gmail.com>
2025-10-30 21:13:36 +0100

    Implement code changes to enhance functionality and improve performance

---
+++ app/src/items.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from player import Player # Gestion de la dépendance circulaire pour eviter les conflits entre les fichiers
+
+#-------------------------------------------Les classes abstraites-----------------------------------------------
+
+class Item(ABC):
+    """ Classe de base abstraite pour tous les items. """
+    def __init__(self, count : int):
+        self.count = count
+    def use(self):
+        pass 
+
+class Food(Item) :
+    def __init__(self, count : int = 1, add_step : int = 0) :
+        super().__init__(count)
+        self.add_step = add_step
+    def use(self):
+        if self.count > 1 :
+            self.count = self.count - 1
+
+
+#---------------------------------------------SECTION : Consommation rapide ------------------------------------
+class Meal(Food):
+    pass
+
+class Banana(Food):
+    pass
+
+class Sandwich(Food):
+    pass
+
+class Apple(Food):
+    pass
+
+class Cake(Food):
+    pass
+
+
+        
