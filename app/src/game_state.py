2933c362021856cb1e7822ccb74a99bf88db1502
moustadraneismael98 <moustadraneismael98@gmail.com>
2025-11-08 00:13:41 +0100

    update: Update of GameState to make it have a new possibility: ROOM_INTERACTION

--- app/src/game_state.py
+++ app/src/game_state.py
@@ -4,6 +4,7 @@ from enum import Enum
 class GameState(Enum):
     ENTER_ROOM = "enter_room"
     CHOOSING_ROOM = "choosing_room"
+    ROOM_INTERACTION = "room_interaction"
     DIGGING = "digging"
     PICK_A_LOCK = "pick_a_lock"
     REDRAWING = "redrawing"
