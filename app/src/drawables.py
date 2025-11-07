1730351d5d8371e1e4b2865b0a2724c2ab87bd86
moustadraneismael98 <moustadraneismael98@gmail.com>
2025-11-08 00:11:39 +0100

    add: Add of drawables.py, a class whose class's attributes are paths to drawables assets involved in the project

---
+++ app/src/drawables.py
@@ -0,0 +1,20 @@
+
+
+
+class Drawables:
+    
+    KEY = "app/assets/drawables/key.png"
+    GEM = "app/assets/drawables/gem.png"
+    GOLD = "app/assets/drawables/gold.png"
+    COIN = "app/assets/drawables/coin.png"
+    HAMMER = "app/assets/drawables/hammer.png"
+    SHOVEL = "app/assets/drawables/shovel.png"
+    DICE = "app/assets/drawables/dice.webp"
+    STEPS = "app/assets/drawables/steps.png"
+    APPLE = "app/assets/drawables/apple.png"
+    METAL_DETECTOR = "app/assets/drawables/metal_detector.png"
+    CAKE = "app/assets/drawables/cake.png"
+    SANDWICH = "app/assets/drawables/sandwich.png"
+    LOCK_PICK_KIT = "app/assets/drawables/lockpick_kit.png"
+    BANANA = "app/assets/drawables/banana.png"
+    RABBIT_FOOT = "app/assets/drawables/rabbit_foot.png"
